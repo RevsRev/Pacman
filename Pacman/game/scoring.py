9f21e559a2d760b454cdefb64026698a58231620
RevsRev <53410878+RevsRev@users.noreply.github.com>
2019-09-07 23:37:00 +0100

    Add files via upload

--- Pacman/game/scoring.py
+++ Pacman/game/scoring.py
@@ -21,12 +21,12 @@ def score_to_string(score, name):
         return name + score_string
 
 def save_score_to_file(score, name):
-    f = open("game/high_scores.txt", "r+")
+    f = open("C:/Users/Eddie Revell/Documents/Learning Python/Games/Pacman/game/high_scores.txt", "r+")
     if f.read() == "":
         f.write(score_to_string(score, name))
     else:
         f.close() #we've read the file so need to reopen it.
-        f = open("game/high_scores.txt", "r+")
+        f = open("C:/Users/Eddie Revell/Documents/Learning Python/Games/Pacman/game/high_scores.txt", "r+")
         file_content = ""
         line_counter = 0
         score_added = False
@@ -51,16 +51,15 @@ def save_score_to_file(score, name):
                     line_counter += 1
         if line_counter<10 and not(score_added):
             file_content = file_content + "\n" + score_to_string(score, name)
-        print(file_content)
         f.close()
-        open("game/high_scores.txt", "w").close()
-        f = open("game/high_scores.txt", "r+")
+        open("C:/Users/Eddie Revell/Documents/Learning Python/Games/Pacman/game/high_scores.txt", "w").close()
+        f = open("C:/Users/Eddie Revell/Documents/Learning Python/Games/Pacman/game/high_scores.txt", "r+")
         f.write(file_content)
     f.close()
 
 def get_low_and_high_scores():
     scores = []
-    f = open("game/high_scores.txt", "r")
+    f = open("C:/Users/Eddie Revell/Documents/Learning Python/Games/Pacman/game/high_scores.txt", "r")
     for line in f:
         scores.append(score_from_string(line))
     f.close()
@@ -73,4 +72,7 @@ def get_low_and_high_scores():
                 min = score
             if score>max:
                 max = score
-        return min, max
+        if len(scores)== 10:
+            return min, max
+        else:
+            return 0, max
